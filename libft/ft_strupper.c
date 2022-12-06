2dd0a1967d1a645b52be2ac2b8ec668ca2a2a76f
Ricky0625 <rickywong0625@gmail.com>
2022-12-06 15:38:25 +0800

    Added new functions
    ft_strlower: Turn string to lowercase
    ft_strupper: Turn string to uppercase

---
+++ libft/ft_strupper.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupper.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/06 15:33:29 by wricky-t          #+#    #+#             */
+/*   Updated: 2022/12/06 15:36:49 by wricky-t         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char    *ft_strupper(char *str)
+{
+    char    *strupper;
+    size_t  len;
+
+    strupper = str;
+    len = ft_strlen(strupper);
+    while (*strupper != '\0')
+    {
+        *strupper = ft_toupper(*strupper);
+        strupper++;
+    }
+    strupper -= len;
+    return (strupper);
+}
