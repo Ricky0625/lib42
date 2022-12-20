142d2985c90d0cf9c3828a432ddbd27be3c7d390
Ricky0625 <rickywong0625@gmail.com>
2022-12-20 11:38:16 +0800

    norminette

--- libft/ft_strtok.c
+++ libft/ft_strtok.c
@@ -6,7 +6,7 @@
 /*   By: wricky-t <wricky-t@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/20 11:09:01 by wricky-t          #+#    #+#             */
-/*   Updated: 2022/12/20 11:31:29 by wricky-t         ###   ########.fr       */
+/*   Updated: 2022/12/20 11:37:46 by wricky-t         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,25 +15,25 @@
 char	*ft_strtok(char *str, const char *delim)
 {
 	static char	*saveptr;
-  	char 		*start;
+	char		*start;
 	char		*end;
 
-  	if (str != NULL)
-    	start = str;
-  	else
-    	start = saveptr;
-  	if (start == NULL)
-    	return NULL;
-  	start += ft_strspn(start, delim);
-  	if (*start == '\0')
-    	return NULL;
-  	end = start + ft_strcspn(start, delim);
-  	if (*end == '\0')
-    	saveptr = NULL;
+	if (str != NULL)
+		start = str;
+	else
+		start = saveptr;
+	if (start == NULL)
+		return (NULL);
+	start += ft_strspn(start, delim);
+	if (*start == '\0')
+		return (NULL);
+	end = start + ft_strcspn(start, delim);
+	if (*end == '\0')
+		saveptr = NULL;
 	else
 	{
-    	*end = '\0';
-    	saveptr = end + 1;
-  	}
+		*end = '\0';
+		saveptr = end + 1;
+	}
 	return (start);
 }
